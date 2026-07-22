c0b2f593fe5831ba09e98790364b11413ed2aed5
Victor Hugo <vhlima1008@gmail.com>
2026-07-22 18:49:53 -0300

    feat(fundamentals): adding the transform and writeable stream

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -18,7 +18,7 @@
 // process.stdin
 //     .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -38,5 +38,21 @@ class OneToHundredStream extends Readable {
     }
 }
 
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MutiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
 new OneToHundredStream()
-    .pipe(process.stdout)
+    .pipe(new InverseNumberStream())
+    .pipe(new MutiplyByTenStream())
